2471df1febdca48a10ad058ff8ee3d30b3c231cb
Niklaus Maurer <nmaurer@nuus.ch>
2011-11-27 18:30:27 +0100

    Added: Feed-Controller

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,6 @@
 Web::Application.routes.draw do
+  get "feed/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Web::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'feed#index'
 
   # See how all your routes lay out with "rake routes"
 

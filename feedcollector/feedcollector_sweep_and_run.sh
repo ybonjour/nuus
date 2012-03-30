30a116ae07964a67c7d61c0bdbff45fc683e23fd
Yves Bonjour <yves.bonjour@gmail.com>
2012-03-30 17:48:17 +0200

    rename

---
+++ feedcollector/feedcollector_sweep_and_run.sh
@@ -0,0 +1,2 @@
+cat sweep_articles_and_words.sql | mysql -u nuus -pnuus
+time python feedcollector.py
---
+++ feedcollector/sweep_articles_and_words.sql
@@ -0,0 +1,6 @@
+delimiter $$
+USE nuus$$
+
+DELETE FROM word_index$$
+DELETE FROM word$$
+DELETE FROM article$$

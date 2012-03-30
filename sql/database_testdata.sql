bd5b9009b6ee4ce233791213afb3f87ecd1dd072
Yves Bonjour <yves.bonjour@gmail.com>
2012-03-30 17:51:02 +0200

    added feeds

--- sql/database_testdata.sql
+++ sql/database_testdata.sql
@@ -4,4 +4,6 @@ INSERT INTO `nuus`.`feed`
 (`Url`)
 VALUES
     ('http://www.hockeyfans.ch/news_portal/news_rss.php'),
-    ('http://feeds.feedburner.com/ScottHanselman')$$
+    ('http://feeds.feedburner.com/ScottHanselman'),
+	('http://phandroid.com/feed/'),
+	('http://feeds.espace.ch/espacenews')$$

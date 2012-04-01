72d677e364cc3f12e641b864d5ba4ed882d81cc0
Yves Bonjour <yves.bonjour@gmail.com>
2012-04-01 19:47:47 +0200

    Adapted similarity measures, working version

--- articlemapper/similarity.py
+++ articlemapper/similarity.py
@@ -1,4 +1,5 @@
 import database
+import math
 
 class Article:
     def __init__(self, id, title, content, feedId, updated, titleWordCount, contentWordCount, language):
@@ -12,13 +13,14 @@ class Article:
         self.language = language
 
 class Similarity:
-    def __init__(self):
+    def __init__(self, db):
         self.similarity_measures = []
-        self.similarity_measures = [(self.feedSimilarity, 0.3),
-                                    (self.languageSimilarity, 0.3),
-                                    (self.titleSimilarity, 0.2),
-                                    (self.contentSimilarity, 0.2) ]
-        self.db = database.Database()
+        self.similarity_measures = [(self.feedSimilarity, 0.1),
+                                    (self.languageSimilarity, 0.1),
+                                    (self.titleSimilarity, 0.4),
+                                    (self.contentSimilarity, 0.4) ]
+        self.db = db
+        self._numArticles = None
         
     def languageSimilarity(self, article1, article2):
         if article1.language == article2.language and article1.language != '-':
@@ -26,20 +28,49 @@ class Similarity:
         else:
             return 0
 
-    def feedSimilarity(self, article1, article2):2 
+    def feedSimilarity(self, article1, article2):
         if article1.feedId == article2.feedId:
             return 1.0
         else:
             return 0.0
+    
+    def titleSimilarity(self, article1, article2):
+        return self.textSimilarity(article1, article2, 1)
+        
+    def contentSimilarity(self, article1, article2):
+        return self.textSimilarity(article1, article2, 0)
+    
+    def termWeight(self, wordId, articleId, inTitle):
+        #TODO: maybe normalize termFrequency? -> Check results
+        return self.termFrequency(wordId, articleId, inTitle)*self.inverseDocumentFrequency(wordId, inTitle)
 
+    def numArticles(self):
+        if self._numArticles == None:
+            self._numArticles = self.db.uniqueScalarOrZero("SELECT COUNT(Id) FROM article")
+        return self._numArticles
+    
+    def calcIDF(self, numArticles, nunDocumentsTermOccursIn):
+        return math.log(1.0*(numArticles + 1) / (nunDocumentsTermOccursIn + 1))
+        
+    def inverseDocumentFrequency(self, wordId, inTitle):
+        numDocumentsWordOccursIn = self.db.uniqueScalarOrZero("SELECT COUNT(Id) FROM word_index WHERE Word=%s AND InTitle=%s", (wordId, inTitle))
+        return self.calcIDF(self.numArticles(), numDocumentsWordOccursIn)
+    
+    def termFrequency(self, wordId, articleId, inTitle):
+        query = "SELECT Count FROM word_index WHERE Article=%s AND Word=%s AND InTitle=%s"
+        return self.db.uniqueScalarOrZero(query, (articleId, wordId, inTitle))
+    
+    def sumSquaredWordWeight(self, articleId, inTitle):
+        sumWordWeight = 0
+        query = "select word, count from word_index where article=%s and InTitle=%s"
+        for word in self.db.iterQuery(query, (articleId, inTitle)):
+            wordId, wordCount = word
+            sumWordWeight += 1.0*wordCount*math.pow(self.termWeight(wordId, articleId, inTitle), 2)
+        return sumWordWeight
+    
     def textSimilarity(self, article1, article2, inTitle):
-        #Build a 1 to 1 mapping between words from article1 and words
-        #words from article2 if they are equal.
-        #e.g. if article1 contains twice the word 'the'
-        #and article2 contains the word 'the' once
-        #then this is counted as 1 common word.
-
-        query = """select w1.Count, w2.Count
+        sumCommonWordWeight = 0
+        queryCommon = """select w1.Word, w1.Count, w2.Count
                     from word_index as w1
                     inner join word_index as w2
                         on w1.Word = w2.Word
@@ -49,46 +80,31 @@ class Similarity:
                             and w1.InTitle=%s
                             and w2.InTitle=%s
                     inner join word on w1.Word = word.Id"""
-            
-        numCommonWords = 0
-        for commonWord in self.db.iterQuery(query, (article1.id, article2.id, inTitle, inTitle)):
-            numCommonWords += min(commonWord[0], commonWord[1])
-                
-        numWordsArticle1 = (article1.titleWordCount if inTitle else article1.contentWordCount)
-        numWordsArticle2 = (article2.titleWordCount if inTitle else article2.contentWordCount)
-        #the maximum amount of words that could be mapped is the number of words in the smaller text
-        numTotalWords = min(numWordsArticle1, numWordsArticle2)
 
-        return 1.0*numCommonWords / numTotalWords
+        for commonWord in self.db.iterQuery(queryCommon, (article1.id, article2.id, inTitle, inTitle)):
+            wordId, wordCount1, wordCount2 = commonWord
+            
+            #The same word can occur in both textes multiple times
+            #the smaller of the both values is the number of times
+            #the two articles have this word in common
+            wordCount = min(wordCount1, wordCount2)
+            
+            weightInArticle1 = self.termWeight(wordId, article1.id, inTitle)
+            weightInArticle2 = self.termWeight(wordId, article2.id, inTitle)
+            
+            sumCommonWordWeight += wordCount*weightInArticle1*weightInArticle2
+
+        normArticle1 = math.sqrt(self.sumSquaredWordWeight(article1.id, inTitle))
+        normArticle2 = math.sqrt(self.sumSquaredWordWeight(article2.id, inTitle))
         
-    def titleSimilarity(self, article1, article2):
-        return self.textSimilarity(article1, article2, 1)
+        #similarity = cos(alpha), alpha: angle between article1 and article2
+        #cos: R -> [-1..1] -> abs(cos(x)) = [0..1]
+        similarity = abs(1.0*sumCommonWordWeight/(normArticle1*normArticle2))
+        return similarity
         
-    def contentSimilarity(self, article1, article2):
-        return self.textSimilarity(article1, article2, 0)
-    
     def articleSimilarity(self, article1, article2):
         similarity = 0
         for similarity_function, weight in self.similarity_measures:
-            print "similarity comp: {0}".format(weight*similarity_function(article1, article2))
-            similarity += weight*similarity_function(article1, article2)
-        
+            similarity_component = weight*similarity_function(article1, article2)
+            similarity += similarity_component
         return similarity
-        
-    def run(self):
-        self.db.connect()
-        try:
-            articles =[]
-            query = """SELECT Id, Title, Content, Feed, Updated, TitleWordCount,
-                        ContentWordCount, Language FROM article"""
-
-            for article in self.db.iterQuery(query):
-                articles.append(Article(article[0], article[1], article[2], article[3],
-                                        article[4], article[5], article[6], article[7]))
-                                        
-            for article1 in articles:
-                for article2 in articles:
-                    if article1.id <= article2.id: continue
-                    print self.articleSimilarity(article1, article2)
-        finally:
-            self.db.close()
--- articlemapper/similarity_test.py
+++ articlemapper/similarity_test.py
@@ -1,5 +1,58 @@
-import similarity
+from similarity import Similarity
+from similarity import Article
+import database
 
-s = similarity.Similarity()
+def testTwoArticles(articleId1, articleId2):
+    db = database.Database()
+    db.connect()
+    try:
+        s = Similarity(db)
 
-s.run()
+        query = """SELECT Id, Title, Content, Feed, Updated, TitleWordCount,
+                            ContentWordCount, Language FROM article WHERE Id IN (%s, %s)"""
+        
+        articles = []
+        for article in db.iterQuery(query, (articleId1, articleId2)):
+            articles.append(Article(article[0], article[1], article[2], article[3],
+                                    article[4], article[5], article[6], article[7]))
+
+        
+        s.articleSimilarity(articles[0], articles[1])
+    finally:
+        db.close()
+
+def testAllArticles():
+    db = database.Database()
+    db.connect()
+    try:
+        s = Similarity(db)
+        articles =[]
+        query = """SELECT Id, Title, Content, Feed, Updated, TitleWordCount,
+                    ContentWordCount, Language FROM article"""
+
+        for article in db.iterQuery(query):
+            articles.append(Article(article[0], article[1], article[2], article[3],
+                                    article[4], article[5], article[6], article[7]))
+        
+        maxSimilarity = 0.0
+        minSimilarity = 1.0
+        sumSimilarity = 0.0
+        articleCount = 0
+        for article1 in articles:
+            for article2 in articles:
+                if article1.id <= article2.id: continue
+                similarity = s.articleSimilarity(article1, article2)
+                sumSimilarity += similarity
+                articleCount += 1
+                maxSimilarity = max(maxSimilarity, similarity)
+                minSimilarity = min(minSimilarity, similarity)
+        print "Average similarity {0}".format(sumSimilarity/articleCount)
+        print "Max similarity {1}".format(maxSimilarity)
+        print "Min similarity {1}".format(minSimilarity)
+    finally:
+        db.close()
+
+        
+#testTwoArticles(2932, 2925)
+testAllArticles()
+

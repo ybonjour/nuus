ad0563e7a4658d5222b5f86bc380889b2313d24d
Yves Bonjour <yves.bonjour@gmail.com>
2012-04-05 16:40:28 +0200

    Clustering parameter adaption

--- articlemapper/clustering_test.py
+++ articlemapper/clustering_test.py
@@ -7,7 +7,7 @@ db.connect()
 try:
     similarity = similarity.Similarity(db)
     #clusterer = clustering.Clusterer(similarity, db, 4)
-    clusterer = clustering.HierarchicalClusterer(similarity, db, 0.2)
+    clusterer = clustering.HierarchicalClusterer(similarity, db, 0.1)
     clusterer.clustering()
 
     db.commit()

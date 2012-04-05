14438adb24ee23a60f45dee5c688debbd9513145
Yves Bonjour <yves.bonjour@gmail.com>
2012-04-05 17:32:19 +0200

    Added HTML output

--- articlemapper/results_clustering_html.py
+++ articlemapper/results_clustering_html.py
@@ -2,19 +2,19 @@ from database import Database
 from similarity import Similarity
 
 def htmlSimilarityMatrix(db, similarity):
-    html = "<table>"
+    html = "<table border=1>"
     articles = [id for (id,) in db.iterQuery("SELECT Id FROM article")]
     print len(articles)
-    html += "<tr>"
+    html += "<tr><td>&nbsp;</td>"
     for article in articles:
         print article
-        html += "<td>{0}</td>".format(article)
+        html += "<td><b>{0}</b></td>".format(article)
     html += "</tr>"
     
     
     for article1 in articles:
         print article1
-        html += "<tr><td>{0}</td>".format(article1)
+        html += "<tr><td><b>{0}</b></td>".format(article1)
         for article2 in articles:
             print article2
             html += "<td>{0:2f}</td>".format(similarity.articleSimilarity(article1, article2))
@@ -23,9 +23,9 @@ def htmlSimilarityMatrix(db, similarity):
     return html
     
 def htmlClusters(db, similarity):
-    html = "<table>"
+    html = "<table border=1>"
     for (cluster,) in db.iterQuery("SELECT id FROM cluster"):
-        html += "<tr><td>{0}</td>".format(cluster)
+        html += "<tr><td><b>{0}</b></td>".format(cluster)
         html += "<td>{0}</td>".format(",".join(str(article) for (article,) in db.iterQuery("SELECT Id FROM article WHERE Cluster=%s", cluster)))
         html += "</tr>"
     html += "</table>"

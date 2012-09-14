f3114e6e8da42a87c1f32e527cbdd5ad47ca3551
nmaurer <nmaurer.dev@gmail.com>
2012-09-14 23:50:23 +0200

    web: trivial stream implementation is working (you have to be logged in though)

--- web/app/assets/javascripts/streams.js
+++ web/app/assets/javascripts/streams.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
-    switchToIndex(0);
 
-    $(".streamArticle").click(function(){
-        switchToIndex($(this).index());
+    $.get('/streams.json', function(data) {
+
+        $("#stream > *").remove();
+
+        $.each(data, function(key, article) {
+           var element = $('<div class="streamArticle">' + article.title + '</div>');
+           element.appendTo("#stream");
+           element.data('content', article.content);
+        });
+
+        switchToIndex(0);
+
+        $(".streamArticle").click(function(){
+            switchToIndex($(this).index());
+        });
+
+        $("body").keydown(function(e){
+            if(e.keyCode === 38){
+                switchToPreviousArticle();
+            } else if(e.keyCode === 40){
+                switchToNextArticle();
+            }
+        });
     });
 
-    $("body").keydown(function(e){
-        if(e.keyCode === 38){
-            switchToPreviousArticle();
-        } else if(e.keyCode === 40){
-            switchToNextArticle();
-        }
-    });
 });
 
 function switchToIndex(index){
@@ -19,6 +32,8 @@ function switchToIndex(index){
     if(element.length === 0) return;
     $(".selected").removeClass("selected");
     element.addClass("selected");
+
+    $('#article').html(element.data('content'));
 }
 
 function switchToPreviousArticle(){

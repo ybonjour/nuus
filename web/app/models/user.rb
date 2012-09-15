dcd24ca18bbddaa4cfd795ce35c297dd2ccb7981
nmaurer <nmaurer.dev@gmail.com>
2012-09-15 13:16:20 +0200

    web: added user validation: email must be unique

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
+
+  validates :email, :uniqueness => true
   validates :password, :presence => true, :on => :create
   
 end

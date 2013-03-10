6d2a8d9b9b71bd1bfdd260ca7529b03f42a4ea56
Yves Bonjour <yves.bonjour@gmail.com>
2013-03-10 20:09:23 +0100

     Services adapted

--- services/clustering/Service.py
+++ services/clustering/Service.py
@@ -1,7 +1,12 @@
 __author__ = 'Yves Bonjour'
+
 from Clusterer import Clusterer
-from ClusterStore import MemoryClusterStore
+from Clusterer import MemoryClusterStore
+from VectorCalculator import VectorCalculator
+from Indexer import Indexer
 
 if __name__ == "__main__":
+    indexer = Indexer()
+    calculator = VectorCalculator(indexer)
     centroid_store = MemoryClusterStore([])
-    clusterer = Clusterer(centroid_store)
+    clusterer = Clusterer(centroid_store, calculator)

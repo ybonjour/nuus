a014d9bf5ed4a3aaa930555ba764170418e96017
Ubuntu <ubuntu@ip-10-56-53-64.eu-west-1.compute.internal>
2013-04-20 15:13:11 +0000

    Added scripts

---
+++ install_indexing.sh
@@ -0,0 +1,4 @@
+rm -rf /usr/local/nuus
+mkdir /usr/local/nuus
+cp -r common /usr/local/nuus
+cp -r services/indexing /usr/local/nuus
---
+++ services/indexing/start_indexing.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=$PYTHONPATH:/usr/local/nuus/indexing:/usr/local/nuus/common
+python Service.py localhost 5000

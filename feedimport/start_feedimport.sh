a4343e109f426478b32e0ef48ef43d203da83138
Ubuntu <ubuntu@ip-10-226-203-17.eu-west-1.compute.internal>
2013-04-22 15:20:59 +0000

    Added install and start script for FeedImporter

---
+++ feedimport/start_feedimport.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=$PYTHONPATH:/usr/local/nuus/feedimport:/usr/local/nuus/common
+python /usr/local/nuus/feedimport/FeedImporter.py /usr/local/nuus/common/nuus_prod.cfg $1
---
+++ install_feedimport.sh
@@ -0,0 +1,5 @@
+mkdir -p /usr/local/nuus
+rm -rf /usr/local/nuus/feedimport
+rm -rf /usr/local/nuus/common
+cp -r common /usr/local/nuus
+cp -r feedimport /usr/local/nuus

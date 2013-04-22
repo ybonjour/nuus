8f2fb11545d4d79373fc6ee14f0fa43dd00aa152
Ubuntu <ubuntu@ip-10-226-203-17.eu-west-1.compute.internal>
2013-04-22 15:14:14 +0000

    Added start and stop scripts for services

---
+++ start_all_services.sh
@@ -0,0 +1,4 @@
+service nuus-indexing start
+service nuus-clustering start
+service nuus-articles start
+service nuus-feeds start
---
+++ stop_all_services.sh
@@ -0,0 +1,4 @@
+service nuus-indexing stop
+service nuus-clustering stop
+service nuus-articles stop
+service nuus-feeds stop
